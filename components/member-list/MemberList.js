a48b064152a319c55f0d7bcb36a428d930bd9aa6
Aaron Nguyen <me@aaronnguyen.dev>
2021-10-19 22:34:23 -0400

    Rename Team to MemberList

---
+++ components/member-list/MemberList.js
@@ -0,0 +1,15 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
+const MemberList = () => {
+  return (
+    <Box>
+      <Typography variant="h5" marginBottom={1}>
+        👋 Say Hi To Us!
+      </Typography>
+    </Box>
+  );
+};
+
+export default MemberList;
