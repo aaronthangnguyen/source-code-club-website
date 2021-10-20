2cc13d6b55a125a7db4f35e2ef120ced379a814a
Aaron Nguyen <me@aaronnguyen.dev>
2021-10-19 22:18:03 -0400

    Redesign layout

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,20 +1,14 @@
-import { Box, Container } from "@mui/material";
+import { AppBar, Box, Container, Grid } from "@mui/material";
 import React from "react";
 import Footer from "../footer/Footer";
 import Navbar from "../navbar/Navbar";
 
 const Layout = ({ children }) => {
   return (
-    <Box minHeight="100vh" display="flex" flexDirection="column">
-      <Box component="header">
-        <Navbar />
-      </Box>
-      <Box component="main">
-        <Container sx={{ mt: 2 }}>{children}</Container>
-      </Box>
-      <Box component="footer" sx={{ mb: 1, mt: "auto" }}>
-        <Footer />
-      </Box>
+    <Box height="100vh" display="flex" flexDirection="column" gap={4}>
+      <Navbar />
+      <Container>{children}</Container>
+      <Footer />
     </Box>
   );
 };
